b47683c5b8451b463bd2c5601a0a0ec212448017
Even Stensberg <evenstensberg@gmail.com>
2026-08-30 13:19:35 +0200

    fix: allow sorting and mutating arrays due perf + bump lockfile (#434)
    * fix: allow sorting and mutating arrays due perf
    
    * chore: bump lockfile

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,8 @@ import configs from "eslint-config-webpack/configs.js";
 export default defineConfig([
   {
     extends: [configs["recommended-dirty"]],
+    rules: {
+      "unicorn/no-array-sort": "off",
+    },
   },
 ]);
